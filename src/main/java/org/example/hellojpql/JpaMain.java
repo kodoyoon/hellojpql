31b661f2f0022d8b7e8d6a3843c324f7010e0b4c
doyoon <doyoun0527@naver.com>
2024-03-20 01:15:12 +0900

    기본 문법과 쿼리 API - 결과조회 API

--- src/main/java/org/example/hellojpql/JpaMain.java
+++ src/main/java/org/example/hellojpql/JpaMain.java
@@ -21,11 +21,7 @@ public class JpaMain {
             em.persist(member);
 
             TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class); //Typequery : 반환 타입이 명확할때 사용
-            List<Member> resultList = query.getResultList(); // 결과가 하나 이상일 때, 리스트 반환, 결과가 없으면 빈 리스트 반환
 
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
 
             tx.commit();
         } catch (Exception e) {
