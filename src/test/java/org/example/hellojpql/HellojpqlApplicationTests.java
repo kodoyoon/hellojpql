2030f70b5c4e8e12e7edf02428eddb4bd0ff0249
doyoon <doyoun0527@naver.com>
2024-03-20 00:44:57 +0900

    init commit

---
+++ src/test/java/org/example/hellojpql/HellojpqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hellojpql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellojpqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
