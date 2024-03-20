a7125f54a95431c32704f7b05091fb379972a96d
doyoon <doyoun0527@naver.com>
2024-03-21 00:56:42 +0900

    조건식(기본 CASE 식)

--- src/main/java/org/example/hellojpql/JpaMain.java
+++ src/main/java/org/example/hellojpql/JpaMain.java
@@ -35,15 +35,19 @@ public class JpaMain {
 
             em.flush();
             em.clear();
-            String query = "select m.username, 'HELLO' , TRUE From Member m";
-            List<Object[]> result = em.createQuery(query)
-                .getResultList();
 
-            for(Object[] objects : result) {
-                System.out.println("objects = " + objects[0]);
-                System.out.println("objects = " + objects[1]);
-                System.out.println("objects = " + objects[2]);
+            String query =
+                "select " +
+                             "case when m.age <= 10 then '학생요금' " +
+                             "      when m.age >= 60 then '경로요금' " +
+                            "       else '일반요금' " +
+                            "end " +
+                             "from Member m";
+         List<String> result =    em.createQuery(query, String.class)
+                    .getResultList();
 
+            for (String s : result) {
+                System.out.println("s = " + s);
             }
 
             tx.commit();
