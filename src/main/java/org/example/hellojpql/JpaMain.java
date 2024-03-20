b84c4a45c9de00c7979467fcdc8784368b02e753
doyoon <doyoun0527@naver.com>
2024-03-21 00:59:46 +0900

    조건식(NULLIF)

--- src/main/java/org/example/hellojpql/JpaMain.java
+++ src/main/java/org/example/hellojpql/JpaMain.java
@@ -25,7 +25,7 @@ public class JpaMain {
 
 
             Member member = new Member();
-            member.setUsername(null);
+            member.setUsername("관리자");
             member.setAge(10);
 
             member.setTeam(team);
@@ -36,7 +36,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select coalesce(m.username, '이름 없는 회원') from Member  m ";
+            String query = "select nullif(m.username, '관리자') from Member  m ";
 
          List<String> result =    em.createQuery(query, String.class)
                     .getResultList();
