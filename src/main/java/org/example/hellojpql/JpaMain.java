3db416fb831e2fa89f9d5e37ac48b49e35073490
doyoon <doyoun0527@naver.com>
2024-03-20 20:03:43 +0900

    프로젝션

--- src/main/java/org/example/hellojpql/JpaMain.java
+++ src/main/java/org/example/hellojpql/JpaMain.java
@@ -20,8 +20,14 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
             List<Member> result = em.createQuery("select m from Member m" , Member.class)
                     .getResultList();
+
+            Member findMember = result.get(0);
+            findMember.setAge(20);
 
             tx.commit();
         } catch (Exception e) {
