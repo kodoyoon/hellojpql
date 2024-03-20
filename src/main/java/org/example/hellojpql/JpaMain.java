b31b51a563dde01d6ef7456709b9f1251f9f3f3c
doyoon <doyoun0527@naver.com>
2024-03-21 00:58:29 +0900

    조건식(COALESCE)

--- src/main/java/org/example/hellojpql/JpaMain.java
+++ src/main/java/org/example/hellojpql/JpaMain.java
@@ -25,7 +25,7 @@ public class JpaMain {
 
 
             Member member = new Member();
-            member.setUsername("teamA");
+            member.setUsername(null);
             member.setAge(10);
 
             member.setTeam(team);
@@ -36,13 +36,8 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query =
-                "select " +
-                             "case when m.age <= 10 then '학생요금' " +
-                             "      when m.age >= 60 then '경로요금' " +
-                            "       else '일반요금' " +
-                            "end " +
-                             "from Member m";
+            String query = "select coalesce(m.username, '이름 없는 회원') from Member  m ";
+
          List<String> result =    em.createQuery(query, String.class)
                     .getResultList();
 
