96a286caeb9f52dfc6a94358a7085f935ab9443e
doyoon <doyoun0527@naver.com>
2024-03-20 21:11:31 +0900

    연관관계가 없는 엔티티 외부 조인

--- src/main/java/org/example/hellojpql/JpaMain.java
+++ src/main/java/org/example/hellojpql/JpaMain.java
@@ -34,7 +34,7 @@ public class JpaMain {
 
             em.flush();
             em.clear();
-            String query = "select m from Member m left join m.team t on  t.name = 'teamA'";
+            String query = "select m from Member m left join Team t on m.username = t.name";
 
            List<Member> result = em.createQuery(query,Member.class)
                .setFirstResult(1)
