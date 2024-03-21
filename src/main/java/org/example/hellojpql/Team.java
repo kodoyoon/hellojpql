79d874e944d42a851e5a91d8199367589bcf0ab4
doyoon <doyoun0527@naver.com>
2024-03-22 00:05:10 +0900

    페치 조인의 특징과 한계(BatchSize)

--- src/main/java/org/example/hellojpql/Team.java
+++ src/main/java/org/example/hellojpql/Team.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import org.hibernate.annotations.BatchSize;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,7 @@ public class Team {
     @Id@GeneratedValue
     private Long id;
     private String name;
+    @BatchSize(size = 100)
     @OneToMany(mappedBy = "team")
     private List<Member> memebrs = new ArrayList<>();
 
